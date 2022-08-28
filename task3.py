2c931f3cf7b291e9d57803286d798df45a16c038
SlavaNice <110192779+vvslavanice@users.noreply.github.com>
2022-08-29 00:25:48 +0200

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,35 @@
+nums1 = [1, 2, 3, 4, 5]
+
+nums2 = [1, -2, 3, -4, 5]
+
+
+def square_nums(nums):
+
+    return [num ** 2 for num in nums]
+
+
+def remove_negatives(nums):
+
+    return [num for num in nums if num > 0]
+
+
+def choose_func(numbers, func1, func2):
+    processed = list(filter(lambda x: x < 0, numbers))
+    if len(processed) == 0:
+        return func1(numbers)
+    else:
+        return func2(numbers)
+
+
+if __name__ == "__main__":
+    print(choose_func(nums1, square_nums, remove_negatives))
+    print(choose_func(nums2, square_nums, remove_negatives))
+
+"""def choose_func(numbers, func1, func2):
+    processed = list(filter(lambda x: x > 0, numbers))
+    if len(processed) == 0:
+        return func1(numbers)
+    else:
+        return func2(numbers)"""
+
+
